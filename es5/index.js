11f62275c9abdbfd2fade782378ac51eeb038002
Mike Romain <mchlrmn@me.com>
2017-06-30 11:55:29 +0200

    added changeColor function

--- es5/index.js
+++ es5/index.js
@@ -9,17 +9,24 @@ var _typechecks = require("./typechecks");
 var _constants = require("./constants");
 
 var PurJus = {
-	addTwoNumbers: function addTwoNumbers(firstNum, secondNum) {
-		return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum + secondNum : _constants.shouldBeNum;
+	maths: {
+		addTwoNumbers: function addTwoNumbers(firstNum, secondNum) {
+			return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum + secondNum : _constants.shouldBeNum;
+		},
+		substractTwoNumbers: function substractTwoNumbers(firstNum, secondNum) {
+			return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum - secondNum : _constants.shouldBeNum;
+		},
+		multiplyTwoNumbers: function multiplyTwoNumbers(firstNum, secondNum) {
+			return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum * secondNum : _constants.shouldBeNum;
+		},
+		divideTwoNumbers: function divideTwoNumbers(firstNum, secondNum) {
+			return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum / secondNum : _constants.shouldBeNum;
+		}
 	},
-	substractTwoNumbers: function substractTwoNumbers(firstNum, secondNum) {
-		return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum - secondNum : _constants.shouldBeNum;
-	},
-	multiplyTwoNumbers: function multiplyTwoNumbers(firstNum, secondNum) {
-		return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum * secondNum : _constants.shouldBeNum;
-	},
-	divideTwoNumbers: function divideTwoNumbers(firstNum, secondNum) {
-		return (0, _typechecks.areTypes)(arguments, ["number", "number"]) ? firstNum / secondNum : _constants.shouldBeNum;
+	style: {
+		changeColor: function changeColor(id, color) {
+			(0, _typechecks.areTypes)(arguments, ["string", "string"]) ? document.getElementById(id).style.color = color : _constants.shouldBeStr;
+		}
 	}
 };
 
--- es6/index.js
+++ es6/index.js
@@ -1,19 +1,26 @@
 import { areTypes } from "./typechecks"
-import { shouldBeNum } from "./constants"
+import { shouldBeNum, shouldBeStr } from "./constants"
 
 const PurJus = {
-	addTwoNumbers(firstNum, secondNum) {
-		return areTypes(arguments, ["number", "number"]) ? firstNum + secondNum : shouldBeNum
+	maths: {
+		addTwoNumbers(firstNum, secondNum) {
+			return areTypes(arguments, ["number", "number"]) ? firstNum + secondNum : shouldBeNum
+		},
+		substractTwoNumbers(firstNum, secondNum) {
+			return areTypes(arguments, ["number", "number"]) ? firstNum - secondNum : shouldBeNum
+		},
+		multiplyTwoNumbers(firstNum, secondNum) {
+			return areTypes(arguments, ["number", "number"]) ? firstNum * secondNum : shouldBeNum
+		},
+		divideTwoNumbers(firstNum, secondNum) {
+			return areTypes(arguments, ["number", "number"]) ? firstNum / secondNum : shouldBeNum
+		}
 	},
-	substractTwoNumbers(firstNum, secondNum) {
-		return areTypes(arguments, ["number", "number"]) ? firstNum - secondNum : shouldBeNum
-	},
-	multiplyTwoNumbers(firstNum, secondNum) {
-		return areTypes(arguments, ["number", "number"]) ? firstNum * secondNum : shouldBeNum
-	},
-  divideTwoNumbers(firstNum, secondNum) {
-    return areTypes(arguments, ["number", "number"]) ? firstNum / secondNum : shouldBeNum
-  }
+	style: {
+		changeColor(id, color) {
+			areTypes(arguments, ["string", "string"]) ? document.getElementById(id).style.color = color : shouldBeStr
+		}
+	}
 }
 
 export default PurJus
